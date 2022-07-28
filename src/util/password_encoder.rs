279e918178c3e2d7337f15836fec650f12bd6e3a
lmmqxyx <1368470804@qq.com>
2022-07-28 23:53:42 +0800

    complete the encode module

--- src/util/password_encoder.rs
+++ src/util/password_encoder.rs
@@ -3,12 +3,16 @@ pub struct PasswordEncoder {}
 impl PasswordEncoder {
     /// 加密明文密码
     pub fn encode(raw_password: &str) -> String {
-        let encode_str = String::from("hello");
-        encode_str
+        let encode_str = md5::compute(raw_password);
+        format!("{:x}", encode_str)
     }
 
-    pub fn verify() -> bool {
-        false
+    pub fn verify(password: &str, raw_password: &str) -> bool {
+        if password.eq(raw_password) {
+            return true;
+        }
+        let hashed = PasswordEncoder::encode(raw_password);
+        password.eq(&hashed)
     }
 }
 
@@ -18,7 +22,13 @@ mod test {
 
     #[test]
     fn test_encode() {
-        let s = PasswordEncoder::encode("123456");
+        let s = PasswordEncoder::encode("123456789");
         println!("{}", s);
+    }
+    #[test]
+    fn test_verify() {
+        assert!(PasswordEncoder::verify("password", "password"));
+        let encode_str = PasswordEncoder::encode("123456");
+        assert!(PasswordEncoder::verify("123456", &encode_str));
     }
 }
