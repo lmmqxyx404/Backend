a48de179d889bf216c4f83766a4314432b5ac259
lmmqxyx <1368470804@qq.com>
2022-07-30 23:55:22 +0800

    optimize the trait

--- src/util/string.rs
+++ src/util/string.rs
@@ -10,3 +10,12 @@ impl isEmptyString for Option<String> {
         }
     }
 }
+
+impl isEmptyString for Option<&str> {
+    fn is_empty(&self) -> bool {
+        match self {
+            Some(s) => s.is_empty(),
+            _ => true,
+        }
+    }
+}
