c521cb5f5c4f8f14eb8b4d2cb6fa45c1fcc669de
lmmqxyx <1368470804@qq.com>
2022-08-05 23:39:01 +0800

    add a reference of context

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,3 +1,23 @@
+use crate::config::config::ApplicationConfig;
+
 /// service 层级
 pub mod sys_config_service;
 
+// service context 必须为 pub,否则 无法给上下文使用
+pub struct ServiceContext {
+    pub config: ApplicationConfig,
+}
+
+impl Default for ServiceContext {
+    fn default() -> Self {
+        let config = ApplicationConfig::default();
+        ServiceContext { config }
+    }
+}
+
+/// 提供一个上下文引用，给其余service 使用
+/// 1. login
+/// 2. cpatcha
+lazy_static! {
+    pub static ref CONTEXT: ServiceContext = ServiceContext::default();
+}
