4b7c114333840d61138f0964344df7dac2ac0612
lmmqxyx <1368470804@qq.com>
2022-09-06 22:47:17 +0800

    add a controller

--- src/controller/sys_user_controller.rs
+++ src/controller/sys_user_controller.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, Responder};
+use actix_web::{web, HttpRequest, Responder};
 
 use crate::domain::dto::sign_in::SignDTO;
 use crate::domain::vo::RespVO;
@@ -8,4 +8,17 @@ use crate::error::Error;
 pub async fn login(arg: web::Json<SignDTO>) -> impl Responder {
     // let login_vo = Err("empty account");
     return RespVO::<()>::from_error(&Error::from("empty account"), "-1").resp_json();
+}
+
+/// 用户信息接口
+pub async fn user_info(req: HttpRequest) -> impl Responder {
+    let token = req.headers().get("access-token");
+    match token {
+        Some(token) => {
+            let token_token=token.to_str().unwrap_or("");
+            // 后续修改
+            return RespVO::<()>::from_error(&Error::from("access token"), "-10").resp_json(),
+        }
+        _ => return RespVO::<()>::from_error(&Error::from("access token"), "-10").resp_json(),
+    }
 }
