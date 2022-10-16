d4b1ab8ceca06d79d3d007664cfd5f044ad79c7e
lmmqxyx <1368470804@qq.com>
2022-10-16 23:15:45 +0800

    opttimize the auth controller

--- src/controller/sys_auth_controller.rs
+++ src/controller/sys_auth_controller.rs
@@ -1,6 +1,7 @@
-use crate::domain::vo::RespVO;
 use crate::domain::dto::auth::SysAuthDTO;
+use crate::domain::vo::RespVO;
+use actix_web::{web, Responder};
 
-pub async fn check(arg: web::Json<SysAuthDTO>) ->impl Responder{
-
+pub async fn check(arg: web::Json<SysAuthDTO>) -> impl Responder {
+    RespVO::from_result(&Ok("uncompleted".to_string())).resp_json()
 }
