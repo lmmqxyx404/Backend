999bfd66d0afbe8df00a17e11e1ebd3839bc9328
lmmqxyx <1368470804@qq.com>
2022-10-26 02:49:03 +0800

    fix the word

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,7 +1,9 @@
+///  切记 任何一个暴露出去的接口都应该打印log日志
+
 /// 图片验证码接口(get)
 pub mod img_verify_controller;
 /// 登录接口(post)
 pub mod sys_user_controller;
 
-// 校验 check 接口
+/// 校验 check 接口
 pub mod sys_auth_controller;
