50c251c753f15b8768979bc5f86e9195b93fd1e1
lmmqxyx <1368470804@qq.com>
2022-10-31 23:52:28 +0800

    Impl a from trait(UserAddDTO ==> SysUser).

--- src/domain/table/tables.rs
+++ src/domain/table/tables.rs
@@ -1,4 +1,7 @@
+use rbatis::object_id::ObjectId;
 use serde::{Deserialize, Serialize};
+
+use crate::{domain::dto::user::UserAddDTO, util::password_encoder::PasswordEncoder};
 
 /// 后台用户表
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -9,4 +12,36 @@ pub struct SysUser {
     pub password: Option<String>,
     /// 用户名登录
     pub name: Option<String>,
+}
+
+/// 转化
+impl From<UserAddDTO> for SysUser {
+    fn from(arg: UserAddDTO) -> Self {
+        SysUser {
+            id: ObjectId::new().to_string().into(),
+            account: arg.account.clone(),
+            password: PasswordEncoder::encode(&arg.password.unwrap_or_default()).into(),
+            name: arg.name.clone(),
+        }
+    }
+}
+
+
+mod test {
+    use crate::domain::table::enums::LoginCheck;
+
+    use super::*;
+
+    #[test]
+    fn convert() {
+        let user = UserAddDTO {
+            account: Some("hello".to_string()),
+            password: Some("String".to_string()),
+            name: Some("String".to_string()),
+            role_id: Some("String".to_string()),
+            login_check: Some(LoginCheck::NoCheck),
+        };
+        let b = SysUser::from(user);
+        println!("{:?}", b);
+    }
 }
