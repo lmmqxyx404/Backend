17aa28195de71903de5f392d9632b5fedc81b9bb
lmmqxyx <1368470804@qq.com>
2022-11-02 22:58:52 +0800

    add a mapper sys_user_role

--- src/domain/mapper/mod.rs
+++ src/domain/mapper/mod.rs
@@ -1,1 +1,2 @@
 pub mod sys_user;
+pub mod sys_user_role;
---
+++ src/domain/mapper/sys_user_role.rs
@@ -0,0 +1,11 @@
+use crate::domain::table::tables::SysUserRole;
+
+/// 后台用户角色表 过程宏
+/// 只有使用了crud！ 这个宏之后，才能生成之后的宏
+crud!(SysUserRole {});
+
+impl_select!(SysUserRole{select_list_in_user_id(user_ids:&[String])=>
+"`where user_id in (`
+trim ',': for _,v in user_ids:
+`#{v},`
+`)`"});
