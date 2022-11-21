b3455cb0d4a16ff3ca6e6f08d8c0640fde591330
lmmqxyx <1368470804@qq.com>
2022-11-21 23:24:14 +0800

    optimize the sys_res struct

--- src/domain/vo/res.rs
+++ src/domain/vo/res.rs
@@ -1,4 +1,7 @@
+use rbatis::rbdc::datetime::FastDateTime;
 use serde::{Deserialize, Serialize};
+
+use crate::domain::table::SysRes;
 
 /// 权限资源表
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -8,4 +11,22 @@ pub struct SysResVO {
     pub name: Option<String>,
     pub permission: Option<String>,
     pub path: Option<String>,
+    pub del: Option<i32>,
+    pub create_date: Option<FastDateTime>,
+    pub childs: Option<Vec<SysResVO>>,
+}
+
+impl From<SysRes> for SysResVO {
+    fn from(arg: SysRes) -> Self {
+        Self {
+            id: arg.id,
+            parent_id: arg.parent_id,
+            name: arg.name,
+            permission: arg.permission,
+            path: arg.path,
+            del: arg.del,
+            create_date: arg.create_date,
+            childs: None,
+        }
+    }
 }
