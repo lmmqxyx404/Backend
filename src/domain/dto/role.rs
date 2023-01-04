538598526f35084b0ba507d7d6b1e9aeef46b5b6
lmmqxyx <1368470804@qq.com>
2023-01-04 23:58:42 +0800

    optimize the dto

--- src/domain/dto/role.rs
+++ src/domain/dto/role.rs
@@ -1,5 +1,17 @@
 use rbatis::sql::PageRequest;
 
+/// 角色分页
+pub struct RolePageDTO {
+    pub page_no: Option<u64>,
+    pub page_size: Option<u64>,
+    pub name: Option<String>,
+}
+
+impl From<&RolePageDTO> for PageRequest {
+    fn from(arg: &RolePageDTO) -> Self {
+        PageRequest::new(arg.page_no.unwrap_or(1), arg.page_size.unwrap_or(10))
+    }
+}
 /// 角色资源分页
 pub struct SysRoleResPageDTO {
     pub page_no: Option<u64>,
