bd518f53d06541da9d914e05655137a6158cfb3c
lmmqxyx <1368470804@qq.com>
2023-01-20 23:51:10 +0800

    complete the controller

--- src/controller/sys_role_controller.rs
+++ src/controller/sys_role_controller.rs
@@ -6,8 +6,9 @@ use crate::{domain::dto::role::SysRoleResPageDTO, service::CONTEXT};
 
 /// 角色分页
 pub async fn page(arg: web::Json<SysRoleResPageDTO>) -> impl Responder {
-    return RespVO::<()>::from_error(&Error::from("access token"), "-10").resp_json();
-    // let vo=CONTEXT.sys_role_service
+    let vo = CONTEXT.sys_role_res_service.page(&arg.0).await;
+    return RespVO::from_result(&vo).resp_json();
+    // return RespVO::<()>::from_error(&Error::from("access token"), "-10").resp_json();
 }
 
 pub async fn add(arg: web::Json<SysRoleResPageDTO>) -> impl Responder {
