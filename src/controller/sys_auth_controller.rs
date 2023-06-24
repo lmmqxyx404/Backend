d38b3de0ab2065074c5e4419bcaf0b58c9ab1fc7
lmmqxyx <1368470804@qq.com>
2023-06-24 23:22:52 +0800

    to impl the auth trait

--- src/controller/sys_auth_controller.rs
+++ src/controller/sys_auth_controller.rs
@@ -4,7 +4,9 @@ use actix_web::{web, Responder};
 
 use crate::service::CONTEXT;
 
+///检测token以及path 是否有效且允许访问
+/// todo: to optimizr the relative fn
 pub async fn check(arg: web::Json<SysAuthDTO>) -> impl Responder {
-    let r = CONTEXT.sys_auth_service.check_auth(arg.0);
+    let r = CONTEXT.sys_auth_service.check_auth(arg.0).await;
     RespVO::from_result(&Ok("uncompleted".to_string())).resp_json()
 }
--- src/service/sys_auth_service.rs
+++ src/service/sys_auth_service.rs
@@ -1,6 +1,10 @@
 use crate::domain::dto::auth::SysAuthDTO;
+use crate::domain::vo::jwt::JWT_Token;
+use crate::error::Result;
 pub struct SysAuthService {}
 
 impl SysAuthService {
-    pub fn check_auth(&self, arg: SysAuthDTO) {}
+    pub async fn check_auth(&self, arg: SysAuthDTO) -> Result<JWT_Token> {
+        todo!()
+    }
 }
